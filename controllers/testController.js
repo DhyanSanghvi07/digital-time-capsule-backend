a77819a7f381860243c5c7e20e6eadc4dc8d1125
DhyanSanghvi <dhyan.sanghvi2209@gmail.com>
2026-01-15 00:36:12 +0530

    Setup the basic backend connection using the cluster key

---
+++ controllers/testController.js
@@ -0,0 +1,5 @@
+const testController = (req, res) => {
+  res.send("Controller is working 🚀");
+};
+
+module.exports = testController;
---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+dotenv.config();
+connectDB();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+
+const testRoutes = require("./routes/testRoutes");
+app.use("/", testRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
